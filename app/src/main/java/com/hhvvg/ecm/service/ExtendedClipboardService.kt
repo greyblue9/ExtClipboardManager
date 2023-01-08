f52221e40f0c41bd97b0eb9cc634084fb5e6c9cd
David Reilly <greyblue9@gmail.com>
2023-01-08 07:26:56 -0500

    Update ExtendedClipboardService.kt

--- app/src/main/java/com/hhvvg/ecm/service/ExtendedClipboardService.kt
+++ app/src/main/java/com/hhvvg/ecm/service/ExtendedClipboardService.kt
@@ -106,7 +106,7 @@ class ExtendedClipboardService(
             android.widget.Toast.makeText(context, data.toString(), 1000).show()
             val uri = android.net.Uri.parse(
                "data:text/plain;base64,".plus(
-                   String(java.util.Base64.getEncoder().encode(text.toByteArray()))))           
+                   String(java.util.Base64.getEncoder().encode(((string)text).toByteArray(kotlin.text.Charsets.UTF_8)))))           
             val intent = android.content.Intent("android.intent.action.SET_CLIP", uri)
             intent.setDataAndType(uri, "text/plain")
             context.startActivity(intent)
